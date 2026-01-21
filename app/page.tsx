f08557aaeb1b6b403c9610aae0bb2315df632f75
Sikboy79 <custombasewelds@yahoo.com>
2026-01-21 09:36:58 -0700

    Enviroment setup. Next.js, tailwind, react icons

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-neutral-900">
+      <h1 className="text-4xl font-bold text-white flex items-center gap-3">
+        Internship 
+      </h1>
+    </main>
+  );
+}
