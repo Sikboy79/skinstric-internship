bd8d5202107aa35f625105af8a7f87be7f81147b
Sikboy79 <custombasewelds@yahoo.com>
2026-01-28 08:20:22 -0700

    live layout adgustments

--- app/components/DiagonalLines.tsx
+++ app/components/DiagonalLines.tsx
@@ -1,23 +1,23 @@
 export default function DiagonalLines() {
   return (
-    <div className="pointer-events-none absolute inset-0 z-0 hidden xl:block">
+    <div className="pointer-events-none absolute inset-0 hidden xl:block z-55">
       {/* LEFT > */}
-      <div className="absolute -top-10 -left-16 w-1/2 h-screen">
+      <div className="absolute -top-15 -left-16 w-1/2 h-screen">
         <div className="absolute top-0 left-0 w-106 ">
-          <div className="absolute top-0 left-0 w-full h-px border-t border-dashed border-gray-300 rotate-45 origin-top-left" />
+          <div className="absolute -top-6 left-0 w-full h-px border-t border-dashed border-gray-300 rotate-45 origin-top-left" />
         </div>
         <div className="absolute bottom-0 left-0 w-106">
-          <div className="absolute bottom-0 left-0 w-full h-px border-t border-dashed border-gray-300 -rotate-45 origin-bottom-left" />
+          <div className="absolute -bottom-6 left-0 w-full h-px border-t border-dashed border-gray-300 -rotate-45 origin-bottom-left" />
         </div>
       </div>
 
       {/* RIGHT < */}
-      <div className="absolute -top-10 -right-18 w-1/2 h-screen">
+      <div className="absolute -top-15 -right-18 w-1/2 h-screen">
         <div className="absolute top-0 right-0 w-106">
-          <div className="absolute top-0 right-0 w-full h-px border-t border-dashed border-gray-300 -rotate-45 origin-top-right" />
+          <div className="absolute -top-6 right-0 w-full h-px border-t border-dashed border-gray-300 -rotate-45 origin-top-right" />
         </div>
         <div className="absolute bottom-0 right-0 w-106">
-          <div className="absolute bottom-0 right-0 w-full h-px border-t border-dashed border-gray-300 rotate-45 origin-bottom-right" />
+          <div className="absolute -bottom-6 right-0 w-full h-px border-t border-dashed border-gray-300 rotate-45 origin-bottom-right" />
         </div>
       </div>
     </div>
